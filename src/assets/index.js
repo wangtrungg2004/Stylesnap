022379dd1fe030adc4f462923a7b6643aec32f32
wangtrungg2004 <wangtrung2004@gmail.com>
2025-11-03 03:27:00 +0700

    logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -12,8 +12,13 @@ import texture2 from "./texture2.jpeg";
 import texture3 from "./texture3.jpeg";
 import texture4 from "./texture4.jpeg";
 import texture5 from "./texture5.jpeg";
+
 import logo1 from "./logo1.png";
 import logo2 from "./logo2.png";
+import logo1 from "./logo3.png";
+import logo2 from "./logo4.png";
+import logo1 from "./logo5.png";
+import logo2 from "./logo6.png";
 
 
 export { fileIcon, swatch, download, logoShirt, stylishShirt, logoControls, textIcon, textureLogoPicker, texture1, texture2, texture3, texture4, texture5, logo1, logo2 };
