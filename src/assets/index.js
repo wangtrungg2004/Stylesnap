d63badbfbb058c35dc46ee51c0201791069e9f1b
wangtrungg2004 <wangtrung2004@gmail.com>
2025-11-03 03:30:11 +0700

    logo fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,10 +15,10 @@ import texture5 from "./texture5.jpeg";
 
 import logo1 from "./logo1.png";
 import logo2 from "./logo2.png";
-import logo1 from "./logo3.png";
-import logo2 from "./logo4.png";
-import logo1 from "./logo5.png";
-import logo2 from "./logo6.png";
+import logo3 from "./logo3.png";
+import logo4 from "./logo4.png";
+import logo5 from "./logo5.png";
+import logo6 from "./logo6.png";
 
 
 export { fileIcon, swatch, download, logoShirt, stylishShirt, logoControls, textIcon, textureLogoPicker, texture1, texture2, texture3, texture4, texture5, logo1, logo2 };
